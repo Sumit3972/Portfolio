c8d0b377c96b334228558d1c4de3f6bbeb132c9f
Sumit3972 <124435686+Sumit3972@users.noreply.github.com>
2023-09-22 21:36:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,13 +17,17 @@
      
     // Dark Mode
     let darkmode = document.querySelector('#darkmode');
-     
-    darkmode.onclick = () => {
-        if(darkmode.classList.contains('bx-moon')){
-            darkmode.classList.replace('bx-moon','bx-sun');
-            document.body.classList.add('active');
-        }else{
-            darkmode.classList.replace('bx-sun','bx-moon');
-            document.body.classList.remove('active');
-        }
+
+// Set dark mode as default
+darkmode.classList.add('bx-sun');
+document.body.classList.add('active');
+
+darkmode.onclick = () => {
+    if (darkmode.classList.contains('bx-moon')) {
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('active');
+    } else {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('active');
     }
+}
